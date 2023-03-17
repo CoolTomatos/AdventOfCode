600304ce2a9f98163fa3c6e2f0cfe1762e622992
Shang Xiang <24667806+CoolTomatos@users.noreply.github.com>
2023-03-17 21:13:35 +0100

    Implemented the Day interface.

---
+++ src/main/java/com/cooltomatos/y2022/Day.java
@@ -0,0 +1,24 @@
+package com.cooltomatos.y2022;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Optional;
+
+public abstract class Day {
+  protected final List<String> input;
+
+  public Day(int day) throws IOException {
+    var inputFileName = String.format("2022/day%02d.txt", day);
+    var resource = getClass().getClassLoader().getResource(inputFileName);
+    var path = Paths.get(Optional.ofNullable(resource).orElseThrow().getFile());
+    input = Files.readAllLines(path);
+  }
+
+  public abstract int part1();
+
+  public abstract int part2();
+
+  public static void main(String[] args) {}
+}
