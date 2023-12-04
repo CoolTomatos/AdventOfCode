1f3ba3a595d5a8e3251eeb68501d4d8be66325d8
CoolTomatos <24667806+CoolTomatos@users.noreply.github.com>
2023-12-04 19:17:58 +0100

    2023.04 Refactored

--- src/main/java/com/cooltomatos/aoc/y2023/d04/Card.java
+++ src/main/java/com/cooltomatos/aoc/y2023/d04/Card.java
@@ -1,16 +1,39 @@
 package com.cooltomatos.aoc.y2023.d04;
 
+import static com.google.common.base.Preconditions.checkState;
+import static com.google.common.collect.ImmutableList.toImmutableList;
+import static com.google.common.collect.ImmutableSet.toImmutableSet;
+
+import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
+import java.util.regex.Pattern;
 
-public record Card(
-    int id, Set<Integer> winningNumbers, List<Integer> numbers, int points, long matches) {
+public record Card(int id, Set<Integer> winningNumbers, List<Integer> numbers, int matches) {
   public Card(int id, Set<Integer> winningNumbers, List<Integer> numbers) {
     this(
         id,
         winningNumbers,
         numbers,
-        numbers.stream().filter(winningNumbers::contains).reduce(0, (l, r) -> l == 0 ? 1 : 2 * l),
-        numbers.stream().filter(winningNumbers::contains).count());
+        (int) numbers.stream().filter(winningNumbers::contains).count());
+  }
+
+  static Card parse(String input) {
+    var matcher = Pattern.compile("Card\\s+(\\d+):\\s+(.*) \\|\\s+(.*)").matcher(input);
+    checkState(matcher.matches());
+    var id = Integer.parseInt(matcher.group(1));
+    var winningNumbers =
+        Arrays.stream(matcher.group(2).split("\\s+"))
+            .map(Integer::parseInt)
+            .collect(toImmutableSet());
+    var numbers =
+        Arrays.stream(matcher.group(3).split("\\s+"))
+            .map(Integer::parseInt)
+            .collect(toImmutableList());
+    return new Card(id, winningNumbers, numbers);
+  }
+
+  int points() {
+    return (int) Math.pow(2, matches - 1);
   }
 }
--- src/test/java/com/cooltomatos/aoc/y2023/d04/DayTest.java
+++ src/test/java/com/cooltomatos/aoc/y2023/d04/DayTest.java
@@ -2,16 +2,24 @@ package com.cooltomatos.aoc.y2023.d04;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class DayTest {
+  private Day day;
+
+  @BeforeEach
+  void setUp() {
+    day = new Day("test", "example");
+  }
+
   @Test
   void part1() {
-    assertThat(new Day("test", "example1").part1()).isEqualTo(13);
+    assertThat(day.part1()).isEqualTo(13);
   }
 
   @Test
   void part2() {
-    assertThat(new Day("test", "example2").part2()).isEqualTo(30);
+    assertThat(day.part2()).isEqualTo(30);
   }
 }
