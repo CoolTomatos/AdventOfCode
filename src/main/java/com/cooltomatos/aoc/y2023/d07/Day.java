2774ff27fc28870ec32d98d6186f7e7473946943
CoolTomatos <24667806+CoolTomatos@users.noreply.github.com>
2023-12-07 11:30:17 +0100

    2023.07 Refactored.

--- src/main/java/com/cooltomatos/aoc/y2023/d07/Day.java
+++ src/main/java/com/cooltomatos/aoc/y2023/d07/Day.java
@@ -1,14 +1,29 @@
 package com.cooltomatos.aoc.y2023.d07;
 
 import static com.google.common.base.Preconditions.checkState;
+import static com.google.common.base.Predicates.not;
 import static com.google.common.collect.ImmutableMap.toImmutableMap;
+import static java.util.Map.Entry.comparingByKey;
+import static java.util.function.Function.identity;
 
 import com.cooltomatos.aoc.AbstractDay;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Streams;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.regex.Pattern;
 
 public class Day extends AbstractDay {
-  private final Map<Hand, Integer> handToBid;
+  private static final List<Character> plainOrder =
+      ImmutableList.of('2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A');
+  private static final Character JOKER = 'J';
+  private static final List<Character> jokerOrder =
+      ImmutableList.of(JOKER, '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A');
+
+  private final Map<String, Integer> handToBid;
 
   public Day(String dir, String file) {
     super(2023, 7, dir, file);
@@ -18,33 +33,76 @@ public class Day extends AbstractDay {
             .peek(matcher -> checkState(matcher.matches()))
             .collect(
                 toImmutableMap(
-                    matcher -> new Hand(matcher.group(1)),
-                    matcher -> Integer.parseInt(matcher.group(2))));
+                    matcher -> matcher.group(1), matcher -> Integer.parseInt(matcher.group(2))));
   }
 
   @Override
   public Integer part1() {
-    var sorted =
-        handToBid.entrySet().stream()
-            .sorted(Map.Entry.comparingByKey(Hand.comparator(false)))
-            .toList();
-    int result = 0;
-    for (int i = 0; i < sorted.size(); ) {
-      result += sorted.get(i).getValue() * ++i;
-    }
-    return result;
+    return solve(handToBid, false);
   }
 
   @Override
   public Integer part2() {
-    var sorted =
-        handToBid.entrySet().stream()
-            .sorted(Map.Entry.comparingByKey(Hand.comparator(true)))
-            .toList();
-    int result = 0;
-    for (int i = 0; i < sorted.size(); ) {
-      result += sorted.get(i).getValue() * ++i;
+    return solve(handToBid, true);
+  }
+
+  private static int solve(Map<String, Integer> handToBid, boolean jokerRule) {
+    return Streams.mapWithIndex(
+            handToBid.entrySet().stream()
+                .sorted(comparingByKey(handComparator(jokerRule)))
+                .map(Entry::getValue),
+            (bid, index) -> bid * ((int) index + 1))
+        .reduce(0, Integer::sum);
+  }
+
+  private static Comparator<String> handComparator(boolean jokerRule) {
+    var cardComparator = cardComparator(jokerRule);
+    return Comparator.<String, HandType>comparing(hand1 -> getHandType(hand1, jokerRule))
+        .thenComparing(hand -> hand.charAt(0), cardComparator)
+        .thenComparing(hand -> hand.charAt(1), cardComparator)
+        .thenComparing(hand -> hand.charAt(2), cardComparator)
+        .thenComparing(hand -> hand.charAt(3), cardComparator)
+        .thenComparing(hand -> hand.charAt(4), cardComparator);
+  }
+
+  private static Comparator<Character> cardComparator(boolean jokerRule) {
+    return Comparator.comparingInt((jokerRule ? jokerOrder : plainOrder)::indexOf);
+  }
+
+  private static HandType getHandType(String hand, boolean jokerRule) {
+    var cardToCount =
+        hand.chars()
+            .mapToObj(value -> (char) value)
+            .collect(toImmutableMap(identity(), unused -> 1, Integer::sum));
+    if (cardToCount.size() == 1) {
+      return HandType.FIVE_OF_A_KIND;
     }
-    return result;
+    if (jokerRule && cardToCount.containsKey(JOKER)) {
+      var replacementComparator =
+          Entry.<Character, Integer>comparingByValue()
+              .thenComparing(Entry::getKey, cardComparator(true));
+      char replacement =
+          Maps.filterKeys(cardToCount, not(JOKER::equals)).entrySet().stream()
+              .max(replacementComparator)
+              .map(Entry::getKey)
+              .orElseThrow();
+      return getHandType(hand.replace(JOKER, replacement), false);
+    }
+    return switch (cardToCount.size()) {
+      case 2 -> cardToCount.containsValue(4) ? HandType.FOUR_OF_A_KIND : HandType.FULL_HOUSE;
+      case 3 -> cardToCount.containsValue(3) ? HandType.THREE_OF_A_KIND : HandType.TWO_PAIR;
+      case 4 -> HandType.ONE_PAIR;
+      default -> HandType.HIGH_CARD;
+    };
+  }
+
+  private enum HandType {
+    HIGH_CARD,
+    ONE_PAIR,
+    TWO_PAIR,
+    THREE_OF_A_KIND,
+    FULL_HOUSE,
+    FOUR_OF_A_KIND,
+    FIVE_OF_A_KIND
   }
 }
--- src/test/java/com/cooltomatos/aoc/y2023/d07/DayTest.java
+++ src/test/java/com/cooltomatos/aoc/y2023/d07/DayTest.java
@@ -1,7 +1,6 @@
 package com.cooltomatos.aoc.y2023.d07;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
