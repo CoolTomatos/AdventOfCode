2c9b5e681ed3f62e228e4fcd54ba9c3c103a126e
CoolTomatos <24667806+CoolTomatos@users.noreply.github.com>
2023-12-15 16:20:00 +0100

    Scaffolding 2023.16

---
+++ src/main/java/com/cooltomatos/aoc/y2023/d16/Day.java
@@ -0,0 +1,19 @@
+package com.cooltomatos.aoc.y2023.d16;
+
+import com.cooltomatos.aoc.AbstractDay;
+
+public class Day extends AbstractDay {
+  public Day(String dir, String file) {
+    super(2023, 16, dir, file);
+  }
+
+  @Override
+  public Integer part1() {
+    return null;
+  }
+
+  @Override
+  public Integer part2() {
+    return null;
+  }
+}
---
+++ src/test/java/com/cooltomatos/aoc/y2023/d16/DayTest.java
@@ -0,0 +1,25 @@
+package com.cooltomatos.aoc.y2023.d16;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class DayTest {
+  private Day day;
+
+  @BeforeEach
+  void setUp() {
+    day = new Day("test", "example");
+  }
+
+  @Test
+  void part1() {
+    assertThat(day.part1()).isNull();
+  }
+
+  @Test
+  void part2() {
+    assertThat(day.part2()).isNull();
+  }
+}
