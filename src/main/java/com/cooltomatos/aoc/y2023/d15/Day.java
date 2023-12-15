7a0c1e0da4e5ee0ff8d8e42f1e14c9fd2f632101
CoolTomatos <24667806+CoolTomatos@users.noreply.github.com>
2023-12-15 14:03:53 +0100

    2023.15 Part 2 finished.

--- src/main/java/com/cooltomatos/aoc/y2023/d15/Day.java
+++ src/main/java/com/cooltomatos/aoc/y2023/d15/Day.java
@@ -1,11 +1,19 @@
 package com.cooltomatos.aoc.y2023.d15;
 
+import static com.google.common.base.Preconditions.checkState;
+
 import com.cooltomatos.aoc.AbstractDay;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Streams;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
 
 public class Day extends AbstractDay {
-  private List<String> steps;
+  private final List<String> steps;
 
   public Day(String dir, String file) {
     super(2023, 15, dir, file);
@@ -18,8 +26,30 @@ public class Day extends AbstractDay {
   }
 
   @Override
-  public Number part2() {
-    return null;
+  public Long part2() {
+    Map<Integer, LinkedHashMap<String, Integer>> boxes = new HashMap<>();
+    steps.stream()
+        .map(Pattern.compile("(\\w+)([-=])([1-9]?)")::matcher)
+        .peek(matcher -> checkState(matcher.matches()))
+        .forEach(
+            matcher -> {
+              var label = matcher.group(1);
+              var box = boxes.computeIfAbsent(hash(label), unused -> new LinkedHashMap<>());
+              switch (matcher.group(2).charAt(0)) {
+                case '-' -> box.remove(label);
+                case '=' -> box.put(label, Integer.parseInt(matcher.group(3)));
+              }
+            });
+    return Maps.transformEntries(
+            boxes,
+            (boxNumber, lenses) ->
+                Streams.mapWithIndex(
+                        lenses.values().stream(),
+                        (focus, index) -> (boxNumber + 1) * (index + 1) * focus)
+                    .reduce(0L, Long::sum))
+        .values()
+        .stream()
+        .reduce(0L, Long::sum);
   }
 
   private static int hash(String word) {
--- src/test/java/com/cooltomatos/aoc/y2023/d15/DayTest.java
+++ src/test/java/com/cooltomatos/aoc/y2023/d15/DayTest.java
@@ -2,12 +2,24 @@ package com.cooltomatos.aoc.y2023.d15;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class DayTest {
+  private Day day;
+
+  @BeforeEach
+  void setUp() {
+    day = new Day("test", "example");
+  }
 
   @Test
   void part1() {
-    assertThat(new Day("test", "example").part1()).isEqualTo(1320);
+    assertThat(day.part1()).isEqualTo(1320);
+  }
+
+  @Test
+  void part2() {
+    assertThat(day.part2()).isEqualTo(145);
   }
 }
