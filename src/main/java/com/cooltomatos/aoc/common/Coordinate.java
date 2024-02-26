a9bc2017e76222ee4e0adabf7d0568b03a3ea98c
CoolTomatos <24667806+CoolTomatos@users.noreply.github.com>
2024-02-26 21:17:40 +0100

    Remove unnecessary changes

--- src/main/java/com/cooltomatos/aoc/common/Coordinate.java
+++ src/main/java/com/cooltomatos/aoc/common/Coordinate.java
@@ -1,10 +1,3 @@
 package com.cooltomatos.aoc.common;
 
-import java.util.Comparator;
-
-public record Coordinate(int x, int y) implements Comparable<Coordinate> {
-  @Override
-  public int compareTo(Coordinate o) {
-    return Comparator.comparingInt(Coordinate::x).thenComparingInt(Coordinate::y).compare(this, o);
-  }
-}
+public record Coordinate(int x, int y) {}
