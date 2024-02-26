afbce72f3aa2ed626a927f4227ec4feb4c1d1caa
CoolTomatos <24667806+CoolTomatos@users.noreply.github.com>
2024-02-26 21:15:46 +0100

    Remove usages of guava classes.

--- src/main/java/com/cooltomatos/aoc/y2023/d16/Day.java
+++ src/main/java/com/cooltomatos/aoc/y2023/d16/Day.java
@@ -2,10 +2,8 @@ package com.cooltomatos.aoc.y2023.d16;
 
 import com.cooltomatos.aoc.AbstractDay;
 import com.cooltomatos.aoc.common.Coordinate;
-import com.google.common.collect.HashMultimap;
-import com.google.common.collect.Lists;
-import com.google.common.collect.SetMultimap;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class Day extends AbstractDay {
@@ -22,90 +20,100 @@ public class Day extends AbstractDay {
   }
 
   @Override
-  public Integer part1() {
+  public Long part1() {
     return solve(new Beam(new Coordinate(0, 0), Direction.RIGHT));
   }
 
-  private int solve(Beam start) {
-    SetMultimap<Coordinate, Direction> visited = HashMultimap.create();
-    var toVisit = Lists.newArrayList(start);
+  private long solve(Beam start) {
+    var toVisit = new ArrayList<Beam>();
+    toVisit.add(start);
+    var visited = new HashSet<Beam>();
     while (!toVisit.isEmpty()) {
-      var first = toVisit.removeFirst();
-      var current = first.coordinate;
-      var sign = grid[current.x()][current.y()];
-      var direction = first.direction;
-      if (!visited.put(current, direction)) {
+      var current = toVisit.removeFirst();
+      var coordinate = current.coordinate;
+      var direction = current.direction;
+
+      if (!visited.add(current)) {
         continue;
       }
-      switch (sign) {
+
+      switch (grid[coordinate.x()][coordinate.y()]) {
         case '|' -> {
           if (direction == Direction.LEFT || direction == Direction.RIGHT) {
-            visited.put(current, Direction.LEFT);
-            visited.put(current, Direction.RIGHT);
-            if (current.x() > 0) {
-              toVisit.add(new Beam(new Coordinate(current.x() - 1, current.y()), Direction.UP));
+            visited.add(new Beam(coordinate, Direction.LEFT));
+            visited.add(new Beam(coordinate, Direction.RIGHT));
+            if (coordinate.x() > 0) {
+              toVisit.add(
+                  new Beam(new Coordinate(coordinate.x() - 1, coordinate.y()), Direction.UP));
             }
-            if (current.x() < grid.length - 1) {
-              toVisit.add(new Beam(new Coordinate(current.x() + 1, current.y()), Direction.DOWN));
+            if (coordinate.x() < grid.length - 1) {
+              toVisit.add(
+                  new Beam(new Coordinate(coordinate.x() + 1, coordinate.y()), Direction.DOWN));
             }
             continue;
           }
         }
         case '-' -> {
           if (direction == Direction.UP || direction == Direction.DOWN) {
-            visited.put(current, Direction.UP);
-            visited.put(current, Direction.DOWN);
-            if (current.y() > 0) {
-              toVisit.add(new Beam(new Coordinate(current.x(), current.y() - 1), Direction.LEFT));
+            visited.add(new Beam(coordinate, Direction.UP));
+            visited.add(new Beam(coordinate, Direction.DOWN));
+            if (coordinate.y() > 0) {
+              toVisit.add(
+                  new Beam(new Coordinate(coordinate.x(), coordinate.y() - 1), Direction.LEFT));
             }
-            if (current.y() < grid[0].length - 1) {
-              toVisit.add(new Beam(new Coordinate(current.x(), current.y() + 1), Direction.RIGHT));
+            if (coordinate.y() < grid[0].length - 1) {
+              toVisit.add(
+                  new Beam(new Coordinate(coordinate.x(), coordinate.y() + 1), Direction.RIGHT));
             }
             continue;
           }
         }
         case '\\' -> {
-          visited.put(current, direction);
-          if (direction == Direction.UP && current.y() > 0) {
-            toVisit.add(new Beam(new Coordinate(current.x(), current.y() - 1), Direction.LEFT));
-          } else if (direction == Direction.DOWN && current.y() < grid[0].length - 1) {
-            toVisit.add(new Beam(new Coordinate(current.x(), current.y() + 1), Direction.RIGHT));
-          } else if (direction == Direction.LEFT && current.x() > 0) {
-            toVisit.add(new Beam(new Coordinate(current.x() - 1, current.y()), Direction.UP));
-          } else if (direction == Direction.RIGHT && current.x() < grid.length - 1) {
-            toVisit.add(new Beam(new Coordinate(current.x() + 1, current.y()), Direction.DOWN));
+          if (direction == Direction.UP && coordinate.y() > 0) {
+            toVisit.add(
+                new Beam(new Coordinate(coordinate.x(), coordinate.y() - 1), Direction.LEFT));
+          } else if (direction == Direction.DOWN && coordinate.y() < grid[0].length - 1) {
+            toVisit.add(
+                new Beam(new Coordinate(coordinate.x(), coordinate.y() + 1), Direction.RIGHT));
+          } else if (direction == Direction.LEFT && coordinate.x() > 0) {
+            toVisit.add(new Beam(new Coordinate(coordinate.x() - 1, coordinate.y()), Direction.UP));
+          } else if (direction == Direction.RIGHT && coordinate.x() < grid.length - 1) {
+            toVisit.add(
+                new Beam(new Coordinate(coordinate.x() + 1, coordinate.y()), Direction.DOWN));
           }
           continue;
         }
         case '/' -> {
-          visited.put(current, direction);
-          if (direction == Direction.UP && current.y() < grid[0].length - 1) {
-            toVisit.add(new Beam(new Coordinate(current.x(), current.y() + 1), Direction.RIGHT));
-          } else if (direction == Direction.DOWN && current.y() > 0) {
-            toVisit.add(new Beam(new Coordinate(current.x(), current.y() - 1), Direction.LEFT));
-          } else if (direction == Direction.LEFT && current.x() < grid.length - 1) {
-            toVisit.add(new Beam(new Coordinate(current.x() + 1, current.y()), Direction.DOWN));
-          } else if (direction == Direction.RIGHT && current.x() > 0) {
-            toVisit.add(new Beam(new Coordinate(current.x() - 1, current.y()), Direction.UP));
+          if (direction == Direction.UP && coordinate.y() < grid[0].length - 1) {
+            toVisit.add(
+                new Beam(new Coordinate(coordinate.x(), coordinate.y() + 1), Direction.RIGHT));
+          } else if (direction == Direction.DOWN && coordinate.y() > 0) {
+            toVisit.add(
+                new Beam(new Coordinate(coordinate.x(), coordinate.y() - 1), Direction.LEFT));
+          } else if (direction == Direction.LEFT && coordinate.x() < grid.length - 1) {
+            toVisit.add(
+                new Beam(new Coordinate(coordinate.x() + 1, coordinate.y()), Direction.DOWN));
+          } else if (direction == Direction.RIGHT && coordinate.x() > 0) {
+            toVisit.add(new Beam(new Coordinate(coordinate.x() - 1, coordinate.y()), Direction.UP));
           }
           continue;
         }
       }
-      if (direction == Direction.UP && current.x() > 0) {
-        toVisit.add(new Beam(new Coordinate(current.x() - 1, current.y()), direction));
-      } else if (direction == Direction.DOWN && current.x() < grid.length - 1) {
-        toVisit.add(new Beam(new Coordinate(current.x() + 1, current.y()), direction));
-      } else if (direction == Direction.LEFT && current.y() > 0) {
-        toVisit.add(new Beam(new Coordinate(current.x(), current.y() - 1), direction));
-      } else if (direction == Direction.RIGHT && current.y() < grid[0].length - 1) {
-        toVisit.add(new Beam(new Coordinate(current.x(), current.y() + 1), direction));
+      if (direction == Direction.UP && coordinate.x() > 0) {
+        toVisit.add(new Beam(new Coordinate(coordinate.x() - 1, coordinate.y()), direction));
+      } else if (direction == Direction.DOWN && coordinate.x() < grid.length - 1) {
+        toVisit.add(new Beam(new Coordinate(coordinate.x() + 1, coordinate.y()), direction));
+      } else if (direction == Direction.LEFT && coordinate.y() > 0) {
+        toVisit.add(new Beam(new Coordinate(coordinate.x(), coordinate.y() - 1), direction));
+      } else if (direction == Direction.RIGHT && coordinate.y() < grid[0].length - 1) {
+        toVisit.add(new Beam(new Coordinate(coordinate.x(), coordinate.y() + 1), direction));
       }
     }
-    return visited.keySet().size();
+    return visited.stream().map(Beam::coordinate).distinct().count();
   }
 
   @Override
-  public Integer part2() {
+  public Long part2() {
     List<Beam> starts = new ArrayList<>();
     for (int i = 0; i < grid.length; i++) {
       starts.add(new Beam(new Coordinate(i, 0), Direction.RIGHT));
@@ -115,7 +123,7 @@ public class Day extends AbstractDay {
       starts.add(new Beam(new Coordinate(0, i), Direction.DOWN));
       starts.add(new Beam(new Coordinate(grid.length - 1, i), Direction.UP));
     }
-    return starts.stream().map(this::solve).reduce(Integer::max).orElse(0);
+    return starts.stream().map(this::solve).reduce(Long::max).orElse(0L);
   }
 
   private record Beam(Coordinate coordinate, Direction direction) {}
@@ -124,6 +132,6 @@ public class Day extends AbstractDay {
     UP,
     DOWN,
     LEFT,
-    RIGHT;
+    RIGHT
   }
 }
