a36b127ec063696aacfbe9d9ca4e29cf1eb3c48f
CoolTomatos <24667806+CoolTomatos@users.noreply.github.com>
2024-11-30 23:34:55 +0100

    Hopefully this year I can do better than last year!

---
+++ src/main/java/com/cooltomatos/aoc/y2024/d01/Day.java
@@ -0,0 +1,19 @@
+package com.cooltomatos.aoc.y2024.d01;
+
+import com.cooltomatos.aoc.AbstractDay;
+
+public class Day extends AbstractDay {
+  public Day(String dir, String file) {
+    super(2024, 1, dir, file);
+  }
+
+  @Override
+  public Number part1() {
+    return null;
+  }
+
+  @Override
+  public Number part2() {
+    return null;
+  }
+}
