c327b7fe02d479a1e8844a4770d38a69afd96fef
CoolTomatos <24667806+CoolTomatos@users.noreply.github.com>
2024-12-02 08:02:00 +0100

    Part 1 finished

--- src/main/java/com/cooltomatos/aoc/y2024/d02/Day.java
+++ src/main/java/com/cooltomatos/aoc/y2024/d02/Day.java
@@ -1,19 +1,42 @@
 package com.cooltomatos.aoc.y2024.d02;
 
 import com.cooltomatos.aoc.AbstractDay;
+import java.util.Arrays;
+import java.util.List;
 
 public class Day extends AbstractDay {
+  private final List<Report> reports;
+
   public Day(String dir, String file) {
     super(2024, 2, dir, file);
+    reports =
+        input.stream()
+            .map(line -> Arrays.stream(line.split("\\s+")).mapToInt(Integer::parseInt).toArray())
+            .map(Report::new)
+            .toList();
   }
 
   @Override
-  public Number part1() {
-    return null;
+  public Long part1() {
+    return reports.stream().filter(Report::safe).count();
   }
 
   @Override
   public Number part2() {
     return null;
+  }
+
+  private record Report(int[] levels) {
+    boolean safe() {
+      boolean increase = levels[0] < levels[1];
+      for (var i = 0; i < levels.length - 1; i++) {
+        if (increase && levels[i] + 1 <= levels[i + 1] && levels[i] + 3 >= levels[i + 1]
+            || !increase && levels[i] - 1 >= levels[i + 1] && levels[i] - 3 <= levels[i + 1]) {
+          continue;
+        }
+        return false;
+      }
+      return true;
+    }
   }
 }
