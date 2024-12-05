a92c33f334a60f4ab4272459716a5652aaa0f592
Shang Xiang <Shang@Uber.com>
2024-12-05 10:04:38 +0100

    Init

---
+++ src/main/java/com/cooltomatos/aoc/y2024/d05/Day.java
@@ -0,0 +1,19 @@
+package com.cooltomatos.aoc.y2024.d05;
+
+import com.cooltomatos.aoc.AbstractDay;
+
+public class Day extends AbstractDay {
+    public Day(String dir, String file) {
+        super(2024, 4, dir, file);
+    }
+
+    @Override
+    public Integer part1() {
+        return null;
+    }
+
+    @Override
+    public Number part2() {
+        return null;
+    }
+}
