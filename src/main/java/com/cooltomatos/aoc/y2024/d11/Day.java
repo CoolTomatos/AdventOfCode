2879f2eeca799327dda927aa98849b34a56de4c2
Shang Xiang <Shang@Uber.com>
2024-12-11 19:28:03 +0100

    Finished part 2

--- src/main/java/com/cooltomatos/aoc/y2024/d11/Day.java
+++ src/main/java/com/cooltomatos/aoc/y2024/d11/Day.java
@@ -1,12 +1,21 @@
 package com.cooltomatos.aoc.y2024.d11;
 
+import static java.util.function.Function.identity;
+
 import com.cooltomatos.aoc.AbstractDay;
-import java.util.ArrayList;
+import com.cooltomatos.aoc.function.EntryFunction;
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.ListMultimap;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Day extends AbstractDay {
   private final List<Long> stones;
+  private final ListMultimap<Long, Long> cache = ArrayListMultimap.create();
 
   public Day(String dir, String file) {
     super(2024, 11, dir, file);
@@ -14,34 +23,45 @@ public class Day extends AbstractDay {
   }
 
   @Override
-  public Integer part1() {
-    List<Long> stones = new ArrayList<>(this.stones);
-    for (int i = 0; i < 25; i++) {
-      blink(stones);
-    }
-    return stones.size();
+  public Long part1() {
+    return solve(25);
   }
 
   @Override
-  public Number part2() {
-    return null;
+  public Long part2() {
+    return solve(75);
   }
 
-  private static void blink(List<Long> stones) {
-    for (int i = stones.size() - 1; i >= 0; i--) {
-      long stone = stones.get(i);
+  private long solve(int times) {
+    Map<Long, Long> stonesAndCount =
+        stones.stream().collect(Collectors.toMap(identity(), stone -> 1L, Long::sum));
+    for (int i = 0; i < times; i++) {
+      stonesAndCount =
+          stonesAndCount.entrySet().stream()
+              .flatMap(
+                  (EntryFunction<Long, Long, Stream<Entry<Long, Long>>>)
+                      (stone, count) ->
+                          blink(stone).stream().map(newStone -> Map.entry(newStone, count)))
+              .collect(Collectors.toMap(Entry::getKey, Entry::getValue, Long::sum));
+    }
+    return stonesAndCount.values().stream().mapToLong(Long::longValue).sum();
+  }
+
+  private List<Long> blink(long stone) {
+    if (!cache.containsKey(stone)) {
       if (stone == 0) {
-        stones.set(i, 1L);
+        cache.put(stone, 1L);
       } else {
         int digits = (int) (Math.floor(Math.log10(stone)) + 1);
         if (digits % 2 == 0) {
-          int pow = (int) Math.pow(10, digits / 2);
-          stones.set(i, stone % pow);
-          stones.add(i, stone / pow);
+          long pow = (long) Math.pow(10, digits / 2);
+          cache.put(stone, stone / pow);
+          cache.put(stone, stone % pow);
         } else {
-          stones.set(i, stone * 2024);
+          cache.put(stone, stone * 2024);
         }
       }
     }
+    return cache.get(stone);
   }
 }
