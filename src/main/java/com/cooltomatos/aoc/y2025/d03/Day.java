9ebbf18f607180cdf68b81dfb21b13e1eca7f7b5
CoolTomatos <24667806+CoolTomatos@users.noreply.github.com>
2025-12-02 12:17:22 +0100

    Init

---
+++ src/main/java/com/cooltomatos/aoc/y2025/d03/Day.java
@@ -0,0 +1,19 @@
+package com.cooltomatos.aoc.y2025.d03;
+
+import com.cooltomatos.aoc.AbstractDay;
+
+public class Day extends AbstractDay {
+  public Day(String dir, String file) {
+    super(2025, 3, dir, file);
+  }
+
+  @Override
+  public Number part1() {
+    return null;
+  }
+
+  @Override
+  public Number part2() {
+    return null;
+  }
+}
